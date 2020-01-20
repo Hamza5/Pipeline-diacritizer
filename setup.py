e830196073eee9cd3d86bad46b6490ff446bf21b
Hamza Abbad <hamza.abbad@gmail.com>
2020-01-20 08:24:18 +0100

    Packaging code without params.

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
         'Topic :: Text Processing :: Linguistic'
     ],
     entry_points={'console_scripts': ['pipeline_diacritizer = pipeline_diacritizer.pipeline_diacritizer.main']},
-    package_data={'': ['Tashkeela_params/*']},
     install_requires=['tensorflow<=1.14.0,>=1.11.0', 'numpy<=1.16.5,>=1.13.0'],
     python_requires='>=3.4,<=3.7'
 )
